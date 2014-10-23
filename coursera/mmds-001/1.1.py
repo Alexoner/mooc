3e65eab260c225a404a94b056e10c83f347277f6
Alexoner <onerhao@gmail.com>
2014-10-23 21:52:09 +0800

    mmds-001:solution script to homework 1

---
+++ coursera/mmds-001/1.1.py
@@ -0,0 +1,12 @@
+import numpy
+
+M = numpy.matrix([[0, 0, 1],
+                 [1.0 / 2.0, 0, 0],
+                 [1.0 / 2.0, 1, 0]])
+e = numpy.matrix([[1.0 / 3.0], [1.0 / 3.0], [1.0 / 3.0]])
+v = numpy.matrix([[1.0], [1.0], [1.0]])
+print v
+
+for i in range(5):
+    v = 0.7 * M * v + (1 - 0.7) * e
+    print 'iteration %d' % (i + 1), v
---
+++ coursera/mmds-001/1.3.py
@@ -0,0 +1,14 @@
+import numpy
+
+M = numpy.matrix([[0, 0, 1],
+                 [1.0 / 2.0, 0, 0],
+                 [1.0 / 2.0, 1, 0]])
+e = numpy.matrix([[1], [1], [1]])
+v = numpy.matrix([[1.0], [1.0], [1.0]])
+print v
+
+for i in range(5):
+    v = M * v
+    print 'iteration %d' % (i + 1), v
+
+print v
