35bb6f067a46f6a66441aaf836d88b533218a7a6
Alexoner <onerhao@gmail.com>
2014-11-16 17:58:20 +0800

    coursera.ml-007:6,regularized linear regression cost function

--- coursera/ml-007/6/mlclass-ex5-007/mlclass-ex5/linearRegCostFunction.m
+++ coursera/ml-007/6/mlclass-ex5-007/mlclass-ex5/linearRegCostFunction.m
@@ -19,8 +19,14 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+h = X * theta
+diff = h - y
 
+%unregularized
+J = 1/(2*m) * diff' * diff
 
+%regularized
+J = J + lambda/(2*m)*theta(2:end)'*theta(2:end)
 
 
 
