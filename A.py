8a0d6781d4d03507938aa241c3c4a116ecfff6ee
Alexoner <onerhao@gmail.com>
2015-12-13 00:07:40 +0800

    nlpintro-001:word sence disambiguation: supervised classification

--- A.py
+++ A.py
@@ -1,6 +1,11 @@
 from main import replace_accented
 from sklearn import svm
 from sklearn import neighbors
+import sys
+import nltk
+import numpy as np
+from scipy import sparse
+import operator
 
 # don't change the window size
 window_size = 10
@@ -24,6 +29,14 @@ def build_s(data):
     s = {}
 
     # implement your code here
+    def get_context_words(lexelt):
+        words = set()
+        map(lambda t: map(lambda w: words.add(w),
+                          list(nltk.word_tokenize(t[1]))[-window_size:] +
+                          list(nltk.word_tokenize(t[3]))[0:window_size+1]),
+            data[lexelt])
+        s[lexelt] = list(words)
+    map(get_context_words, data)
 
     return s
 
@@ -48,7 +61,19 @@ def vectorize(data, s):
     labels = {}
 
     # implement your code here
-
+    def vectorize_one(t):
+        context_vector = map(lambda x: 0, s)
+        try:
+            map(lambda word: operator.setitem(context_vector, s.index(word), context_vector[s.index(word)]+1),
+                list(nltk.word_tokenize(t[1]))[-window_size:] +
+                list(nltk.word_tokenize(t[3]))[0:window_size+1])
+        except Exception as e:
+            # print data, s
+            print 'word', 'not in s ', e
+            pass
+        vectors[t[0]] = context_vector
+        labels[t[0]] = t[4]
+    map(vectorize_one, data)
     return vectors, labels
 
 
@@ -80,7 +105,17 @@ def classify(X_train, X_test, y_train):
     svm_clf = svm.LinearSVC()
     knn_clf = neighbors.KNeighborsClassifier()
 
-    # implement your code here
+    # sparse.csr_matrsparse.csr_matrsparse.csr_matrixixiximplement your code here
+    # X_train_arr = sparse.csr_matrix(np.array(X_train.values()))
+    # y_train_arr = sparse.csr_matrix(np.array(y_train.values()))
+    # X_test_arr = sparse.csr_matrix(np.array(X_test.values()))
+    X_train_arr = np.array(X_train.values())
+    y_train_arr = np.array(y_train.values())
+    X_test_arr = np.array(X_test.values())
+    svm_clf.fit(X_train_arr, y_train_arr)
+    knn_clf.fit(X_train_arr, y_train_arr)
+    svm_results = zip(X_test.keys(), svm_clf.predict(X_test_arr))
+    knn_results += zip(X_test.keys(), knn_clf.predict(X_test_arr))
 
     return svm_results, knn_results
 
@@ -96,6 +131,15 @@ def print_results(results ,output_file):
     # implement your code here
     # don't forget to remove the accent of characters using main.replace_accented(input_str)
     # you should sort results on instance_id before printing
+    with open(output_file, 'w') as f:
+        try:
+            map(lambda item: map(lambda t:
+                                f.write(item[0]+' '+t[0]+' '+t[1]+'\n'), item[1]),
+                sorted(results.items(), cmp))
+        except Exception as e:
+            print e
+            import ipdb; ipdb.set_trace()  # XXX BREAKPOINT
+            pass
 
 # run part A
 def run(train, test, language, knn_file, svm_file):
@@ -106,6 +150,7 @@ def run(train, test, language, knn_file, svm_file):
         X_train, y_train = vectorize(train[lexelt], s[lexelt])
         X_test, _ = vectorize(test[lexelt], s[lexelt])
         svm_results[lexelt], knn_results[lexelt] = classify(X_train, X_test, y_train)
+        print 'predicted results:', svm_results[lexelt], knn_results[lexelt]
 
     print_results(svm_results, svm_file)
     print_results(knn_results, knn_file)
