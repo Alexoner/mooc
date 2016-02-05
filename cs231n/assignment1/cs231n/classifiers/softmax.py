8ae17c8f88739e7278f45e35e7d712100ea4fec1
Alexoner <onerhao@gmail.com>
2016-02-05 17:43:35 +0800

    softmax vectorized loss and gradient

--- cs231n/assignment1/cs231n/classifiers/softmax.py
+++ cs231n/assignment1/cs231n/classifiers/softmax.py
@@ -27,17 +27,15 @@ def softmax_loss_naive(W, X, y, reg):
   (C, D) = W.shape
   N  = X.shape[1]
   scores = W.dot(X)
+  T = np.zeros((C, N))
+  T[y, np.arange(N)] = 1
   for i in xrange(N):
     try:
       scores_i = scores[:, i]
-      logP = scores[:, i][y[i]] - \
-              np.max(scores_i) - \
-              np.log(np.sum(np.exp(scores_i - np.max(scores_i))))
-      loss_i = -logP
-      dloss_i = np.dot(
-          (scores_i - (np.arange(C) == y[i])). reshape(
-              (C, 1)),
-          X[:, i].reshape((1, X[:, i].shape[0])))
+      P_i = softmax(scores_i)
+      logP_i = np.log(P_i)
+      loss_i = -logP_i[y[i]]
+      dloss_i = np.dot((P_i - T[:, i]). reshape((C, 1)), X[:, i].reshape((1, D)))
       loss += loss_i
       dW += dloss_i
     except Exception as e:
@@ -79,9 +77,41 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
+  (D, N) = X.shape
+  C = W.shape[0]
+
+  # C x N matrix, the linear (inner/dot) product
+  Z = W.dot(X)
+  # the target value
+  P = softmax(Z, axis=0)
+  # multiplied by 1-of-K coding target value to get cross-entropy loss
+  T = np.zeros_like(P)
+  T[y, np.arange(N)] = 1
+  loss = np.sum(-np.log(P)[y, np.arange(N)])
+  dW = (P - T).dot(X.T)
+
+  # Right now the loss is a sum over all training examples, but we want it
+  # to be an average instead so we divide by num_train.
+  loss /= N
+  dW /= N
+
+  # regularization term
+  RW = 0.5 * reg * np.sum(W * W)
+  dRW = reg * W
+  loss += RW
+  dW += dRW
   pass
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
 
   return loss, dW
+
+
+def softmax(Z, axis=0):
+  # the softmax function
+  Z_max = np.max(Z, axis=axis)
+  Z_normalized = Z - Z_max
+  logP = Z_normalized - np.log(np.sum(np.exp(Z_normalized), axis=axis))
+  P = np.exp(logP)
+  return P
